6c6c7d193d61b1f6c3c60df4a19eee759f5cc8a3
Cezar Cocu <me@cezarcocu.com>
2013-04-10 01:20:38 -0400

    Made improvements to db:migrate, removed erb files, cleaned up board controller and its views

--- db/migrate/20130318234747_create_users.rb
+++ db/migrate/20130318234747_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.index :id
       t.string :name
       t.string :email
       t.string :type
-
       t.timestamps
     end
   end
