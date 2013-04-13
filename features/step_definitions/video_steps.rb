35fa801839fe1cede6424dc9d9d270a1c93d2737
Samson <samsonyang2142@gmail.com>
2013-04-13 17:28:37 -0400

    Add features

---
+++ features/step_definitions/video_steps.rb
@@ -0,0 +1,24 @@
+Given(/^the page is set up$/) do
+  pending
+end
+
+Given(/^I am logged in as a User$/) do
+  pending
+end
+
+Then(/^I should see the field for: (.*)$/) do |field|
+  pending
+end
+
+When(/^I fill in the field for: (.*)$/) do |field|
+  pending
+end
+
+Then(/^I should (not )?be able to see embedded YouTube video$/) do |logic|
+  pending
+end
+
+Then(/^I should see the description of video$/) do
+  pending
+end
+
