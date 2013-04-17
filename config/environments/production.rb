208799e1b1e3187085dd5415f7d837a3e7a0073c
Cezar Cocu <me@cezarcocu.com>
2013-04-17 15:12:16 -0700

    Fixed Merge Conflicts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 BTVProject::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.assets.manifest = Rails.root.join("public/assets")
   # Code is not reloaded between requests
   config.cache_classes = true
 
