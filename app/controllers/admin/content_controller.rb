03760d31f0f94489d55b9cabbdb31126f95c7696
SimbaHuang <xhuang3@binghamton.edu>
2013-04-23 15:05:24 -0400

    Added users table for admin/show_users

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -1,9 +1,15 @@
 module Admin; end
 class Admin::ContentController < ApplicationController
   def index
+    @admin = true
+    if @admin
+    else
+      redirect_to :controller => "/home", :action => "index"
+    end
   end
 
   def show_users
+    @users = User.find(:all)
   end
 
   def show_requests
