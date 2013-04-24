81573161293fb99861461a069885152e935e9423
Cezar Cocu <cezar@Cezars-MacBook-Pro.local>
2013-04-24 00:48:10 -0400

    Added Popover feature on Mouse Hover for Board Page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,2 +1,17 @@
 //Place main Javascript code here
 
+$(function ()  // Written By the Board Page team
+{ 	
+	if(window.location.pathname === "/board" ) { //Only execute this code on the /board page
+		$(".thumbnail").hover( //only catch items class span4
+  		function () {
+		    var text = $(this).find("a").attr("description");
+		    $(this).popover({ content: text, trigger: "manual"});
+		    $(this).popover("show");
+        },
+	    function () {
+	    	$(this).popover("hide");
+	    }
+    );	
+	}
+});
