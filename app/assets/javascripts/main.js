a520de103d7ab5f245c10df98125f5bf3fea07fa
Cezar Cocu <cezar@Cezars-MacBook-Pro.local>
2013-04-25 02:50:25 -0400

    Fixed some popover bugs

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,24 +3,23 @@
 $(function ()  // Written By the Board Page team
 { 	
 	if(window.location.pathname === "/board" ) { //Only execute this code on the /board page
-		var size = $("li.span4").size();
+		
+
+		var size = $("li.span4").size();  
 		var start = 0;
 		var end = 3;
 		var that = $("li.span4");
-		while(end < size +3){
+		while(end < size +3){  //Wraps every third thumbnail in a ul
 			console.log(end);
 			that.slice(start, end).wrapAll('<ul class="thumbnails" />');
 			start = end;
 			end +=3;
-
 		}
-
-
 
 		$(".thumbnail").hover( //only catch items class span4
   		function () {
 		    var text = $(this).find("a").attr("description");
-		    $(this).popover({ content: text, trigger: "manual"});
+		    $(this).popover({ content: text, trigger: "manual", placement: "top"});
 		    $(this).popover("show");
         },
 	    function () {
