874e581d5f880be5805e6412ad7b99bddeeeb29d
saasbook <saasbook@saasbook.(none)>
2013-04-28 20:19:45 -0700

    Executive Board

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ user1 = User.create!(:name => "Scott Sanders", email => "Treasurer@TheNewBTV.com
 BoardInfo.create!(:user_id => user1.id, :title=> "Treasurer", :board => "executive", :photo => "/assets/Contact.jpg", :description => "The Newly Elected Treasurer of BTV!")
 
 user1 = User.create!(:name => "Chris Graf", email => "ProductionManager@TheNewBTV.com")
-BoardInfo.create!(:user_id => user1.id, :title => "Production Manager", board => "executive", :photo => "/assets/Contact.jpg", :description => "The Newly Elected Production Manager of BTV!")
+BoardInfo.create!(:user_id => user1.id, :title => "Production Manager", board => "executive", :photo => "/assets/Contact.jpg", :description => "The Newly Elected Production Manager of BTV!!")
 
 user1 = User.create!(:name => "Joseph Bzomowski", email => "programming@TheNewBTV.com")
 BoardInfo.create!(:user_id => user1.id, :title => "Administrative Director", :board => "executive", :photo => "/assets/Contact.jpg", :description => "The Newly Elected Administrative Director of BTV!")
