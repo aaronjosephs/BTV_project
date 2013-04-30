ecb926c33cf0ef02a2b2388807de6fda2d25a575
Michael Sosa <m.sosa1127@gmail.com>
2013-04-30 13:24:39 -0700

    Feature for more/less

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -2,16 +2,34 @@ class ScheduleController < ApplicationController
   def index
 		@cal = Google::Calendar.new(:username => 'fauxbtv@gmail.com',
                            :password => 'bingbtv2013')
-		t =Time.now
+		@current_time = Time.now
+		@t =Time.now
 		@block = 60*60*12
-		t = t + (@block)
-		@cal = @cal.find_events_in_range(Time.now,t )
+		@t = @t + (@block)
+		@cal = @cal.find_events_in_range(@current_time,@t )
 		while(@cal ==nil)
-			t = t + (@block)
-			@cal = @cal.find_events_in_range(Time.now,t )
+			@t = @t + (@block)
+			@cal = @cal.find_events_in_range(@current_time,@t )
 		end
 		@tmp = @cal.find_all { |e| e and e.start_time }
 		@cal = @tmp.uniq.sort_by { |obj| obj.start_time }
   end
+
+	def more
+		@block = 60*60*12
+		@t = @t + @block
+		@cal = @cal.find_events_in_range(@current_time,@t )
+		@tmp = @cal.find_all { |e| e and e.start_time }
+		@cal = @tmp.uniq.sort_by { |obj| obj.start_time }
+	end
+
+	def moreLess
+		@block = 60*60*12
+		@current_time = @current_time - @block
+		@cal = @cal.find_events_in_range(@current_time,@t )
+		@tmp = @cal.find_all { |e| e and e.start_time }
+		@cal = @tmp.uniq.sort_by { |obj| obj.start_time }
+	end
 end
+		
 
