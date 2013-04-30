6c02c712d4992efd87b784f72b52825513c95992
Michael Sosa <m.sosa1127@gmail.com>
2013-04-30 13:15:40 -0700

    Implemented getting 12 hour schedule

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -2,6 +2,16 @@ class ScheduleController < ApplicationController
   def index
 		@cal = Google::Calendar.new(:username => 'fauxbtv@gmail.com',
                            :password => 'bingbtv2013')
+		t =Time.now
+		@block = 60*60*12
+		t = t + (@block)
+		@cal = @cal.find_events_in_range(Time.now,t )
+		while(@cal ==nil)
+			t = t + (@block)
+			@cal = @cal.find_events_in_range(Time.now,t )
+		end
+		@tmp = @cal.find_all { |e| e and e.start_time }
+		@cal = @tmp.uniq.sort_by { |obj| obj.start_time }
   end
 end
 
