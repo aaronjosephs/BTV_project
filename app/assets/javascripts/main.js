3ba7b84475077609524aa4af630346e7f7620629
Cezar Cocu <me@cezarcocu.com>
2013-05-06 12:03:29 -0700

    Added AJAx function to load latest Youtube video async

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,12 @@
 //Place main Javascript code here
-
 $(function ()  // Written By the Board Page team
 { 	
 	if(window.location.pathname === "/board" ) { //Only execute this code on the /board page
-		
-
 		var size = $("li.span4").size();  
 		var start = 0;
 		var end = 3;
 		var that = $("li.span4");
-		while(end < size +3){  //Wraps every third thumbnail in a ul
-			console.log(end);
+		while(end < size+3){  //Wraps every third thumbnail in a ul
 			that.slice(start, end).wrapAll('<ul class="thumbnails" />');
 			start = end;
 			end +=3;
