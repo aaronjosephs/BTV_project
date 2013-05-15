dae502472aeaf77479cc73e080c8c1aa994b9faf
Cezar Cocu <me@cezarcocu.com>
2013-05-14 20:02:00 -0700

    Fixed some issues with menu bar not displaying correctly, added differnt background to schedule

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,19 +1,15 @@
 class ScheduleController < ApplicationController
   def index
+  		@schedule_active = "active"
 		@cal = Google::Calendar.new(:username => 'fauxbtv@gmail.com',
                            :password => 'bingbtv2013', :calendar => 'BTV' )
 		@current_time = Time.now
 		@t =Time.now
-		@block = 60*60*3
+		@block = 60*60*8
 		@current_time = @current_time - @block
 		@t = @t + (@block)
 		@schedule = @cal.find_events_in_range(@current_time,@t )
-		#@count = 0
-		#while(@schedule.nil? or @count < 3)
-			#@count = @count + 1
-			#@t = @t + (@block)
-			#@schedule = @cal.find_events_in_range(@current_time,@t )
-		#end
+
 		if @schedule.nil?	
 			@schedule = 1		
 		else		
